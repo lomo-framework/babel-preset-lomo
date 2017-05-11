c3a9a9bc396ab9ad7cc50f67c96e535d58b33705
Vincent <const@lomo.cc>
2017-05-11 23:37:20 +0800

    fix:createElement

--- index.js
+++ index.js
@@ -2,10 +2,9 @@
 
 module.exports = {
     presets: [
-        require('babel-preset-es2015'),
         require('babel-preset-flow')
     ],
     plugins: [
-        [require('babel-plugin-transform-react-jsx'), { pragma: "Lomo.createElement" }],
+        [require('babel-plugin-transform-react-jsx'), { pragma: "createElement" }],
     ]
 };
